8fb0ff48836876d029e4028660f4dfa045520b46
SvetlanaShel <sveta61shel169@gmail.com>
2025-04-13 14:55:20 +0200

    add commit

--- src/pages/UserData/UserData.tsx
+++ src/pages/UserData/UserData.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import {
+  UserDataContainer,
+  UserDataCard,
+  UserDataName,
+  UserDataInfo,
+  Image,
+  LogoutButton,
+  ErrorText,
+} from './styles';
 import { useUserContext } from '../../context/UserContext';
-import * as S from './styles';
-
 const UserData: React.FC = () => {
-  const { user } = useUserContext();
-
-  if (!user) return <div>Нет данных пользователя</div>;
-
+  const { user, setUser } = useUserContext();
+  const navigate = useNavigate();
+  const [randomUser, setRandomUser] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const USER_API_URL = 'https://randomuser.me/api/';
+  const fetchRandomUser = async () => {
+    setError(null);
+    try {
+      const response = await axios.get(USER_API_URL);
+      setRandomUser(response.data.results[0]);
+    } catch (err) {
+      setError('Произошла ошибка при загрузке данных');
+    }
+  };
+  useEffect(() => {
+    fetchRandomUser();
+  }, []);
+  const handleLogout = () => {
+    setUser(null);
+    navigate('/');
+  };
+  if (!user) {
+    return <UserDataContainer>Нет данных о текущем пользователе</UserDataContainer>;
+  }
   return (
-    <S.Container>
-      <h2>{user.name}</h2>
-      <p>Email: {user.email}</p>
-    </S.Container>
+    <UserDataContainer>
+      <UserDataCard>
+        {randomUser && (
+          <Image src={randomUser.picture.large} alt="Random User" />
+        )}
+        <UserDataName>{user.name}</UserDataName>
+        <UserDataInfo>Email: {user.email}</UserDataInfo>
+        {randomUser && (
+          <>
+            <UserDataInfo>
+              Страна: {randomUser.location.country}
+            </UserDataInfo>
+            <UserDataInfo>
+              Город: {randomUser.location.city}
+            </UserDataInfo>
+          </>
+        )}
+        {error && <ErrorText>{error}</ErrorText>}
+        <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
+      </UserDataCard>
+    </UserDataContainer>
   );
 };
-
 export default UserData;
-
